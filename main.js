52c6cc8cecee000a55f636871df011891217be87
liusongbai <liusongbai2072@qq.com>
2022-06-21 17:40:47 +0800

    feat: update

--- main.js
+++ main.js
@@ -10,20 +10,26 @@ chrome.runtime.onMessage.addListener(
       const div = document.createElement('div')
       div.innerText = `${text}`
       div.style.position = 'fixed'
-      div.style.top = '10%'
+      div.style.top = '0'
       div.style.left = '50%'
       div.style.transform = 'translateX(-50%)'
       div.style.padding = '10px'
       div.style.borderRadius = '5px'
-      div.style.backgroundColor = 'rgba(237, 237, 237, 0.8)'
+      div.style.backgroundColor = 'rgba(239, 250, 244, 1)'
+      div.style.border = '1px solid rgb(112, 201, 149)'
       div.style.transition = 'all 0.5s'
+      div.style.zIndex = 999
       document.body.appendChild(div)
       setTimeout(() => {
-        div.style.opacity = '0'
-      }, 1000)
+        div.style.top = '10%'
+      }, 10)
+      setTimeout(() => {
+        div.style.top = '-10%'
+      }, 3000)
       setTimeout(() => {
         div.style.display = 'none'
-      }, 1500)
+        document.body.removeChild(div)
+      }, 3500)
       text = text.replace(/[\u200b-\u200f\uFEFF\u202a-\u202e]/g, '')
       navigator.clipboard.writeText(text)
       text = ''
